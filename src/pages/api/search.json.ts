2b37d2dab33d42a01565d3a7e2d46cf723ab2ca8
Robeen <robeeen@gmail.com>
2025-04-07 17:16:02 +0600

    remove tolowercase

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -25,21 +25,21 @@ export const GET: APIRoute = async ({ url }): Promise<Response> => {
   );
 
   // Filter articles based on query
-  const searchResults = allBlogArticles.filter((article) => {
-    const titleMatch: boolean = article.data.title
-      .toLowerCase()
-      .includes(query!.toLowerCase());
+  // const searchResults = allBlogArticles.filter((article) => {
+  //   const titleMatch: boolean = article.data.title
+  //     .toLowerCase()
+  //     .includes(query!.toLowerCase());
 
-    const bodyMatch: boolean = article.body
-      .toLowerCase()
-      .includes(query!.toLowerCase());
+  //   const bodyMatch: boolean = article.body
+  //     .toLowerCase()
+  //     .includes(query!.toLowerCase());
 
-    const slugMatch: boolean = article.slug
-      .toLowerCase()
-      .includes(query!.toLowerCase());
+  //   const slugMatch: boolean = article.slug
+  //     .toLowerCase()
+  //     .includes(query!.toLowerCase());
 
-    return titleMatch || bodyMatch || slugMatch;
-  });
+  //   return titleMatch || bodyMatch || slugMatch;
+  // });
 
   return new Response(JSON.stringify(searchResults), {
     status: 200,
