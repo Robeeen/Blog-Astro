9349430c4c6da08d6f1fd707d0ca6806672c014a
Robeen <robeeen@gmail.com>
2025-04-07 14:46:22 +0600

    search.json and search.astro

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -25,18 +25,21 @@ export const GET: APIRoute = async ({ url }): Promise<Response> => {
   );
 
   // Filter articles based on query
+  
   const searchResults = allBlogArticles.filter((article) => {
+
+    const safeQuery = query?.toLowerCase() || "";
     const titleMatch: boolean = article.data.title
       .toLowerCase()
-      .includes(query!.toLowerCase());
+      .includes(safeQuery);
 
     const bodyMatch: boolean = article.body
       .toLowerCase()
-      .includes(query!.toLowerCase());
+      .includes(safeQuery);
 
     const slugMatch: boolean = article.slug
       .toLowerCase()
-      .includes(query!.toLowerCase());
+      .includes(safeQuery);
 
     return titleMatch || bodyMatch || slugMatch;
   });
